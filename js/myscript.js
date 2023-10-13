b407076806272d1efb0f52104803f78b9c3ff8af
nodirbek <n.gulomov@hamkorbank.uz>
2023-10-13 15:25:53 +0500

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+// const ism =prompt('Ismiz nima ?');
+// const fam =prompt('Familyangiz nima ?');
+// console.log(`${ism} ${fam}`);
+
+
+// const ism =`Nodibek`;
+// const fam ="G'ulomov";
+// console.log(`${ism} ${fam}`);
+
+// let a=10;
+// let b=10;
+
+// console.log(++a);
+// console.log(--b);
+let a=25;
+console.log(5*5===25);
